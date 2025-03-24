f84c285e87a40e38716f0264c4e6777184fd24c0
ayush <ak4426280@gmail.com>
2025-03-24 18:17:14 +0530

    env variable updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,14 +9,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBLsiYDbzyD8Qr2rEzdPJuD7Jy7y50eK7Q",
-    authDomain: "intervuai-b72ee.firebaseapp.com",
-    projectId: "intervuai-b72ee",
-    storageBucket: "intervuai-b72ee.firebasestorage.app",
-    messagingSenderId: "443825173909",
-    appId: "1:443825173909:web:649bca063333b02ec4b29c",
-    measurementId: "G-Y0NH7ZLZJL"
+
+
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
 
 // Initialize Firebase
